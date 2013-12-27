0468e537064baac5a88e25777852d551206e80b6
beiminglei <584573318@qq.com>
2013-12-27 14:20:01 +0800

    FeatureA

--- src/com/android_fans/test/MainActivity.java
+++ src/com/android_fans/test/MainActivity.java
@@ -2,14 +2,23 @@ package com.android_fans.test;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+	
+	private TextView btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btn = (TextView) findViewById(R.id.btn);
+		btn.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +26,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		startActivity(new Intent(MainActivity.this, BActivity.class));
 	}
 
 }
