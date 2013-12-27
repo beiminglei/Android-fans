4370be570b902e43de1a0f3dac279a016014d4c7
beiminglei <584573318@qq.com>
2013-12-27 14:51:57 +0800

    message for your changes. Lines starting dljfklsdjfk
    with '#' will be ignored, and an empty message aborts the commit.

--- src/com/android_fans/test/BActivity.java
+++ src/com/android_fans/test/BActivity.java
@@ -2,14 +2,21 @@ package com.android_fans.test;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
-public class BActivity extends Activity {
+public class BActivity extends Activity implements OnClickListener {
+	private TextView btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_b);
+		btn = (TextView) findViewById(R.id.btn);
+		btn.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +24,12 @@ public class BActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.b, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		startActivity(new Intent(BActivity.this, B2Activity.class));
 	}
 
 }
