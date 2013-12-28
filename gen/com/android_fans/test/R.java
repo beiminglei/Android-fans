805711d12e05517f79980dc850071f0757d372e9
beiminglei <584573318@qq.com>
2013-12-28 11:55:26 +0800

    CCCCC

--- gen/com/android_fans/test/R.java
+++ gen/com/android_fans/test/R.java
@@ -30,12 +30,14 @@ public final class R {
     public static final class layout {
         public static final int activity_b=0x7f030000;
         public static final int activity_b2=0x7f030001;
-        public static final int activity_main=0x7f030002;
+        public static final int activity_b3=0x7f030002;
+        public static final int activity_main=0x7f030003;
     }
     public static final class menu {
         public static final int b=0x7f070000;
         public static final int b2=0x7f070001;
-        public static final int main=0x7f070002;
+        public static final int b3=0x7f070002;
+        public static final int main=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
